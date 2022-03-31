6a7a61e9498cf144ce031418ed9a0722289e711b
Kavya Goudar <65352079+Kavya2716@users.noreply.github.com>
2022-03-31 15:41:02 +0530

    Add files via upload
    Reading only input tags

---
+++ InputTagDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandlingDemo
+{
+    internal class InputTagDisplay
+    {
+        public void Input()
+        {
+            FileStream fileStream = new FileStream(@"F:\NC4\mydata.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+            while(reader.Peek() > 0)
+            {
+                string tag = reader.ReadLine();
+                if(tag.Contains("<input"))
+                {
+                    Console.WriteLine(tag);
+                }
+            }
+            Console.ReadLine();
+            reader.Close();
+            fileStream.Close();
+        }
+    }
+}
